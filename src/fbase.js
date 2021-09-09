78a05e348b8373c43d31b3a63466f02177a76fda
NamMinwu <minwoo000625@naver.com>
2021-09-09 14:18:16 +0900

    initallizing

---
+++ src/fbase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvAGLARf0fmptE8Ts5FX2LiTJhsQ3ipws",
+    authDomain: "nwitter-a2eec.firebaseapp.com",
+    projectId: "nwitter-a2eec",
+    storageBucket: "nwitter-a2eec.appspot.com",
+    messagingSenderId: "662961662982",
+    appId: "1:662961662982:web:1e42a4527745e8790fe787"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
